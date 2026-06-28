155729d77638251760bc0f28b1d1db8eebafa347
0ortcloud2466 <0ortcloud2466@gmail.com>
2026-06-28 20:27:00 +0900

    Linux

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./pages/Router.tsx";
-import { ThemeProvider } from "./components/themeProvider.tsx";
+import { ThemeProvider } from "./components/ThemeProvider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
